66a604bc4efa21f82169bc23002da14e08e86c06
Jitendra <jitendra.kondru@gmail.com>
2021-11-02 23:34:55 -0500

    added table

--- routes/apple.js
+++ routes/apple.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('apple', { title: 'Search Results' });
+  res.render('apple', { title: 'Search Results by Apple' });
 });
 
 module.exports = router;
